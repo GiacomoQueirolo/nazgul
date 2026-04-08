bd8de7db5ef5d833134a54c9157758102bbb498d
Giacomo Queirolo <giacomo.queirolo@umontpellier.fr>
2026-04-08 17:33:22 +0200

    change in philo: now considering all possible projections

--- src/nazgul/create_all_lenses.py
+++ src/nazgul/create_all_lenses.py
@@ -1,11 +1,18 @@
 # Script to sistematically create all available GL 
 import numpy as np
-from nazgul.mount_doom.generate_particle_lens_dom import wrapper_get_all_lens
+from nazgul.mount_doom.generate_gal_lens import wrapper_get_all_lens
 
-all_lenses = wrapper_get_all_lens(kw_galpart={"min_z":.4,
-                                       "max_z":0.51,
+from pyinstrument import Profiler
+profiler = Profiler()
+profiler.start()
+all_lenses = wrapper_get_all_lens(kw_galpart={"min_z":.09,
+                                       "max_z":0.101,
                                        "min_mass":1e12},
-                           reload=False)
+                           reload=True,
+                            _test=False)
+
+profiler.stop()
+print(profiler.output_text(color=True,show_all=False))
 
 print("Stat: \n")
 print("Lenses: "+str(len(all_lenses)))
