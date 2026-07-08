e7e1027c9a152b9acdb07ad55570f4c026f83ce7
Giacomo Queirolo <giacomo.queirolo@umontpellier.fr>
2026-07-08 15:59:55 +0200

    check_chi2_res.py: minor output to check chi2 results

---
+++ src/nazgul/check_chi2_res.py
@@ -0,0 +1,16 @@
+import numpy as np
+from glob import glob
+from los_res_study import get_model_plot,load_kwargs_result,get_red_chi2
+from pathlib import Path
+
+chi2 = []
+for g in glob("results/models/simNoShear/snap_27_*/kw_res.dll"):
+    model_res_dir = Path(g).parent
+    kwargs_result  = load_kwargs_result(model_res_dir)
+    try:
+        model_plot     = get_model_plot(model_res_dir,kwargs_result=kwargs_result)
+        chi2.append(get_red_chi2(model_plot,verbose=False))
+    except Exception as e:
+        print(f"Failed {e}:\n{model_res_dir}")
+
+print(np.array(chi2))
